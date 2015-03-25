4599857250acd8351494df44c415e2956cabdf15
boweeb <boweeb@gmail.com>
2015-03-25 18:15:02 -0400

    Start with CLI scaffold

--- yaterbit/yaterbit.py
+++ yaterbit/yaterbit.py
@@ -1,1 +1,35 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+'''yaterbit
+
+Usage:
+  yaterbit ship new <name>...
+  yaterbit ship <name> move <x> <y> [--speed=<kn>]
+  yaterbit ship shoot <x> <y>
+  yaterbit mine (set|remove) <x> <y> [--moored|--drifting]
+  yaterbit -h | --help
+  yaterbit --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  --speed=<kn>  Speed in knots [default: 10].
+  --moored      Moored (anchored) mine.
+  --drifting    Drifting mine.
+'''
+
+from __future__ import unicode_literals, print_function
+from docopt import docopt
+
+__version__ = "0.5"
+__author__ = "Jesse Butcher"
+__license__ = "MIT"
+
+
+def main():
+    '''Main entry point for the yaterbit CLI.'''
+    args = docopt(__doc__, version=__version__)
+    print(args)
+
+if __name__ == '__main__':
+    main()
